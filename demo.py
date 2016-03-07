3a5bd1e904be2803bc44e6aa2baa4c30d341dc72
donsingh <dbmsph@gmail.com>
2016-03-07 13:37:29 +0800

    Added Else and Shortened Comments

--- demo.py
+++ demo.py
@@ -22,10 +22,24 @@ while True:
 	if not line:
 		break
 	line = line.replace(" ", "")
-	print ">>"+line
-	#get NODE_ID
 	node = int(line[6:10],16)
-	if(node>25)
+	if(node>25) #SINGLE-PHASE
+            vrms = int(line[18:20],16)
+            vrms += (long(int(line[20:22],16))/100)   #get VRMS values
+            irms = int(line[22:24],16)
+            irms += (long(int(line[24:26],16))/100)   #get IRMS values
+            frqy = int(line[26:28],16)
+            frqy += (long(int(line[28:30],16))/100)	  #get FREQUENCY values
+            pwrf = int(line[30:32],16)
+            pwrf += (long(int(line[32:34],16))/100)   #get POWERFACTOR values
+            actp = int(line[34:38],16)
+            actp += (long(int(line[38:40],16))/100)   #get ACTP values
+            reap = int(line[40:44],16)
+            reap += (long(int(line[44:46],16))/100)   #get REAP values
+            appp = int(line[46:50],16)
+            appp += (long(int(line[50:52],16))/100)   #get APPP values
+            insert_to_db(node,vrms,irms,frqy,pwrf,actp,reap,appp) #INSERT FOR SINGLE PHASE
+        else #added else statement
             #get VRMS values
             vrms = int(line[18:20],16)
             vrms += (long(int(line[20:22],16))/100)
@@ -48,5 +62,3 @@ while True:
             appp = int(line[46:50],16)
             appp += (long(int(line[50:52],16))/100)
             insert_to_db(node,vrms,irms,frqy,pwrf,actp,reap,appp)
-        else
-            
