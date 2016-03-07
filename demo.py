b61779fce7258e0d37c143941fc3afd541a7a94a
donsingh <dbmsph@gmail.com>
2016-03-07 13:57:56 +0800

    Added GetData Function for cleaner code

--- demo.py
+++ demo.py
@@ -17,48 +17,32 @@ def insert_to_db(node,vrms,irms,frqy,pwrf,actp,reap,appp):
 	db.close()
 	return;
 
+#Parse and Convert
+
+def getData(hex, start, end, dtype):
+	data = (dtype==0)?int(hex[start:end],16):long(int(hex[start:end],16)/100)
+	return data;
+
 while True:
 	line = sys.stdin.readline()
 	if not line:
 		break
 	line = line.replace(" ", "")
 	node = int(line[6:10],16)
-	if(node>25) #SINGLE-PHASE
-            vrms = int(line[18:20],16)
-            vrms += (long(int(line[20:22],16))/100)   #get VRMS values
-            irms = int(line[22:24],16)
-            irms += (long(int(line[24:26],16))/100)   #get IRMS values
-            frqy = int(line[26:28],16)
-            frqy += (long(int(line[28:30],16))/100)	  #get FREQUENCY values
-            pwrf = int(line[30:32],16)
-            pwrf += (long(int(line[32:34],16))/100)   #get POWERFACTOR values
-            actp = int(line[34:38],16)
-            actp += (long(int(line[38:40],16))/100)   #get ACTP values
-            reap = int(line[40:44],16)
-            reap += (long(int(line[44:46],16))/100)   #get REAP values
-            appp = int(line[46:50],16)
-            appp += (long(int(line[50:52],16))/100)   #get APPP values
-            insert_to_db(node,vrms,irms,frqy,pwrf,actp,reap,appp) #INSERT FOR SINGLE PHASE
-        else #added else statement
-            #get VRMS values
-            vrms = int(line[18:20],16)
-            vrms += (long(int(line[20:22],16))/100)
-            #get IRMS values
-            irms = int(line[22:24],16)
-            irms += (long(int(line[24:26],16))/100)
-            #get FREQUENCY values
-            frqy = int(line[26:28],16)
-            frqy += (long(int(line[28:30],16))/100)
-            #get POWERFACTOR values
-            pwrf = int(line[30:32],16)
-            pwrf += (long(int(line[32:34],16))/100)
-            #get ACTP values
-            actp = int(line[34:38],16)
-            actp += (long(int(line[38:40],16))/100)
-            #get REAP values
-            reap = int(line[40:44],16)
-            reap += (long(int(line[44:46],16))/100)
-            #get APPP values
-            appp = int(line[46:50],16)
-            appp += (long(int(line[50:52],16))/100)
-            insert_to_db(node,vrms,irms,frqy,pwrf,actp,reap,appp)
+	if(node>25) #Single Phase
+		vrms = getData(line,18,20,0)
+		vrms +=getData(line,20,22,1)   #get VRMS values
+		irms = getData(line,22,24,0)
+		irms +=getData(line,24,26,1)   #get IRMS values
+		frqy = getData(line,26,28,0)
+		frqy +=getData(line,28,30,1)   #get FREQUENCY values
+		pwrf = getData(line,30,32,0)
+		pwrf +=getData(line,32,34,1)   #get POWERFACTOR values
+		actp = getData(line,34,38,0)
+		actp +=getData(line,38,40,1)   #get ACTP values
+		reap = getData(line,40,44,0)
+		reap +=getData(line,44,46,1)   #get REAP values
+		appp = getData(line,46,50,0)
+		appp +=getData(line,50,52,1)   #get APPP values
+		insert_to_db(node,vrms,irms,frqy,pwrf,actp,reap,appp) #INSERT FOR SINGLE PHASE
+	#else #added else statement for Three Phase
